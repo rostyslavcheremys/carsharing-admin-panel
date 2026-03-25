f87d56b305e20ece0713fe19e90a038567315d76
rostikcheremys <rostikcheremys@gmail.com>
2026-03-25 22:27:46 +0200

    add approveUser and getUser methods to UserService

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,5 +1,6 @@
 import { httpsCallable } from "firebase/functions";
-import { functions } from "../firebase";
+import { doc, updateDoc, getDoc } from "firebase/firestore";
+import { functions, db } from "../firebase";
 
 const deleteUserCallable = httpsCallable(functions, "deleteUser");
 
@@ -7,5 +8,26 @@ export class UserService {
     static async deleteUser(userId){
         const result = await deleteUserCallable({ userId });
         return result.data;
+    }
+
+    static async approveUser(userId) {
+        const userRef = doc(db, "users", userId);
+        await updateDoc(userRef, {
+            verificationStatus: "approved",
+        });
+    }
+
+    static async getUser(id) {
+        const userRef = doc(db, "users", id);
+        const userSnap = await getDoc(userRef);
+
+        if (!userSnap.exists()) {
+            throw new Error("User not found");
+        }
+
+        return {
+            id: userSnap.id,
+            ...userSnap.data()
+        };
     }
 }
