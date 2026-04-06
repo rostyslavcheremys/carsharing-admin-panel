5a25a4cd9f357db1e22363094b0135bda7e9f298
rostikcheremys <rostikcheremys@gmail.com>
2026-04-06 20:49:55 +0300

    fix: add disabled state styles to AppButton

--- src/components/Buttons/AppButton.jsx
+++ src/components/Buttons/AppButton.jsx
@@ -3,10 +3,9 @@ import { Button } from "../../libs/mui";
 export const AppButton = ({ className, label, type, onClick, disabled }) => {
     return (
         <Button
-            className={`app-button ${className || ''}`}
+            className={`app-button ${className || ''} ${disabled ? 'disabled' : ''}`}
             type={type}
             onClick={onClick}
-            disabled={disabled}
         >
             {label}
         </Button>
