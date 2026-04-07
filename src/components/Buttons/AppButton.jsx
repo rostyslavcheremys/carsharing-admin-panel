25bb2c1536cf8a01778114305e17a043ba66055e
rostikcheremys <rostikcheremys@gmail.com>
2026-04-07 14:01:01 +0300

    fix: update AppButton

--- src/components/Buttons/AppButton.jsx
+++ src/components/Buttons/AppButton.jsx
@@ -3,9 +3,10 @@ import { Button } from "../../libs/mui";
 export const AppButton = ({ className, label, type, onClick, disabled }) => {
     return (
         <Button
-            className={`app-button ${className || ''} ${disabled ? 'disabled' : ''}`}
+            className={`app-button ${className || ''}`}
             type={type}
             onClick={onClick}
+            disabled={disabled}
         >
             {label}
         </Button>
